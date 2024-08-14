d293d4ccf330d699081cf7067c9d4a09193f4f93
Min Thant Mg <minthant180@gmail.com>
2024-08-14 21:34:32 +0630

    fix:delete duplicate fun

--- app.js
+++ app.js
@@ -17,60 +17,6 @@ app.get('/',(req,res) =>{
     res.status(200).json('Hello from ecommerce Api')
 })
 
-app.post('/register',async (req,res) =>{
-    try {
-        const userData = await User.create(req.body);
-        res.status(200).json(userData)
-    }catch (error){
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.get('/users',async (req,res) =>{
-    try {
-        const users = await User.find({})
-        res.status(200).json(users)
-    }catch (error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-app.get('/user/:id',async (req,res)=>{
-    try {
-        const {id} = req.params
-        if (!mongoose.isValidObjectId(id)) {
-            return res.status(400).json({ message: 'Invalid user ID' });
-        }
-        const userById = await User.findById(id)
-        if(userById == null){
-            res.status(302).json({message: "Invalid user ID"})
-        }
-        res.status(200).json(userById)
-    }catch (error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-app.put('/user/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (!mongoose.isValidObjectId(id)) {
-            return res.status(400).json({ message: 'Invalid user ID' });
-        }
-        const userById = await User.findByIdAndUpdate(id, req.body);
-
-        if (!userById) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        const updatedUser = await User.findById(id);
-        res.status(200).json(updatedUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
 mongoose.connect(MongoDbClient)
     .then(() => console.log('Connected!'))
     .catch(() => console.log('Connection Failed'))
