acc6ef432392fdcaf913ba044437b9828fc73b2e
Minthantmg <minthant180@gmail.com>
2024-08-15 18:25:50 +0900

    fix:update product model

--- models/product.js
+++ models/product.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose')
 
 const ProductSchema = mongoose.Schema({
-    category: String,
-    name: String,
-    description : String,
-    product_image : String,
-    brand: String,
-    price : Number
+    "car_ID" : Number,
+    "symboling" : Number,
+    "CarName" : String,
+    "fueltype" : String,
+    "aspiration" : String,
+    "doornumber" : String,
+    "carbody" : String,
+    "drivewheel" : String,
+    "enginelocation" : String,
+    "wheelbase" : Number,
+    "carlength":Number,
+    "carwidth" :Number,
+    "carheight" : Number,
+    "curbweight" : Number,
+    "enginetype":String,
+    "cylindernumber": String,
+    "enginesize": Number,
+    "fuelsystem" : String,
+    "boreratio":Number,
+    "stroke":Number,
+    "compressionratio":Number,
+    "horsepower":Number,
+    "peakrpm":Number,
+    "citympg":Number,
+    "highwaympg":Number,
+    "price":Number
 })
 
-const Product = mongoose.model("Product",ProductSchema)
+const Product = mongoose.model("Product", ProductSchema)
 module.exports = Product;
