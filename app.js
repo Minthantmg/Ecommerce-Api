b4f29618a726be4726b87bff3a00aadaa328acbd
Minthantmg <minthant180@gmail.com>
2024-08-26 18:31:50 +0900

    fix:update vercel.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const User = require("./models/user");
 const productRoutes = require('./route/product.route');
+const Product = require("./models/product");
 require('dotenv').config();
 
 app.use(express.json())
@@ -15,6 +16,15 @@ app.use('/product',productRoutes)
 
 app.get('/',(req,res) =>{
     res.status(200).json('Hello from ecommerce Api')
+})
+
+app.get('/products',async (req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    }catch (error){
+        res.status(500).json({message:error.message})
+    }
 })
 
 mongoose.connect(MongoDbClient)
