94a3f9de05aee75e3d2926a25d1a2cd81aa10614
Min Thant Mg <minthant180@gmail.com>
2024-09-01 00:09:50 +0630

    fix:cors update

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-const express = require('express')
-var cors = require('cors')
-const index = express()
-index.use(cors())
+const express = require('express');
+var cors = require('cors');
+const index = express();
 const mongoose = require('mongoose');
-const User = require("./models/user");
 const productRoutes = require('./route/product.route');
 const Product = require("./models/product");
 
 require('dotenv').config();
-index.use(express.json())
-index.use(express.urlencoded({extended: false}))
-
+index.use(express.json());
+index.use(express.urlencoded({ extended: false }));
 
 const MongoDbClient = process.env.MONGO_URI;
 
-//routes
-index.use('/product',productRoutes)
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 
-index.get('/',(req, res) =>{
-    res.status(200).json('Hello from ecommerce Api')
-})
+index.use(cors(corsOptions));
 
-index.get('/products',async (req, res) => {
+// Routes
+index.use('/product', productRoutes);
+
+index.get('/', (req, res) => {
+    res.status(200).json('Hello from ecommerce Api');
+});
+
+index.get('/products', async (req, res) => {
     try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    }catch (error){
-        res.status(500).json({message:error.message})
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
 mongoose.connect(MongoDbClient)
     .then(() => console.log('Connected!'))
-    .catch(() => console.log('Connection Failed'))
+    .catch(() => console.log('Connection Failed'));
 
-index.listen(4000,() => {
-    console.log("Server is running on port 4000")
-})
+index.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
 
-module.exports = index
+module.exports = index;
